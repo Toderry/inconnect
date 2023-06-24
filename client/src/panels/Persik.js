de81968d0a5ae77a37830539bd4c57d44d3f9133
ijo42 <admin@Ijo42.ru>
2023-06-24 23:12:50 +0500

    chore: getUsers example

--- client/src/panels/Persik.js
+++ client/src/panels/Persik.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
@@ -6,18 +6,31 @@ import { Panel, PanelHeader, PanelHeaderBack } from '@vkontakte/vkui';
 import persik from '../img/persik.png';
 import './Persik.css';
 import {ROUTES} from "../routes";
+import {getUsers} from "../http/userAPI";
 
-const Persik = ({setActiveStory, id}) => (
-	<Panel id={id}>
-		<PanelHeader
-			id="persik"
-			before={<PanelHeaderBack onClick={() => setActiveStory(ROUTES.PROFILE)} data-to="profile"/>}
-		>
-			Абоба
-		</PanelHeader>
-		<img className="Persik" src={persik} alt="Persik The Cat"/>
-	</Panel>
-);
+const Persik = ({setActiveStory, id}) => {
+	const [users, setUsers] = useState();
+	useEffect(async () => {
+		async function fetchData() {
+			setUsers(await getUsers());
+		}
+
+		await fetchData();
+	}, []);
+
+	return (
+		<Panel id={id}>
+			<PanelHeader
+				id="persik"
+				before={<PanelHeaderBack onClick={() => setActiveStory(ROUTES.PROFILE)} data-to="profile"/>}
+			>
+				Абоба
+				{JSON.stringify(users)}
+			</PanelHeader>
+			<img className="Persik" src={persik} alt="Persik The Cat"/>
+		</Panel>
+	);
+};
 
 Persik.propTypes = {
 	id: PropTypes.string.isRequired,
